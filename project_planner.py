e2924b329c495b7074c276afdc428be1c7cf6f56
jharris49 <22jharris@wakatipu.school.nz>
2026-05-05 13:07:10 -0400

    Grid UI and Task Saving with Support Class
    New Features:
    - Overhauled add_task method using a list of Task objects rather than saving widgets to a list. Method no longer deals with outputting tasks.
    - New method, output_tasks gets, formats, and outputs task details from  each task object in the tasks list.
    
    UI Changes:
    - Added labels to make a grid like UI.
    - Added labels to make the UI simpler to use.
    - Specific grid numbers for UI.
    
    Other:
    - Small blocks of code set up for future additions.

--- project_planner.py
+++ project_planner.py
@@ -3,80 +3,137 @@ from tkinter import messagebox
 
 
 class Task:
-    def __init__(self, task_name, deadline, description, status):
-        self.task_name = task_name
-        self.deadline = deadline
-        self.description = description
+    def __init__(self, t_name, status="Unstarted", description=None):
+        self.t_name = t_name
         self.status = status
+        self.description = description
+        
 
 class ProjectBoardGUI:
     def __init__(self, parent):
+    # Lists
         self.tasks = [
 
         ]
 
-        self.task_labels = [
-
+        self.statuses = [
+            "Unstarted",
+            "In progress",
+            "Finished"
         ]
-
-        self.task_checkboxes = [
-
-        ]
-
+        
+    # Frames setup
         self.current_frame = 1
 
         self.tasks_frame = Frame(parent)
         self.task_entry_frame = Frame(parent)
         
-    # Tasks Frame #
+    #### Tasks Frame ####
         self.task_button = Button(
             self.tasks_frame,
-            text="Add new task",
+            text="+",
             command=self.switch_frame
         )
-        self.task_button.grid() # Show
+        self.task_button.grid(row=len(self.tasks) + 1, column=0) # Show
 
+        self.task_no_header = Label(
+            self.tasks_frame,
+            text="No.",
+            borderwidth=1, 
+            relief="solid",
+            padx=5
+        )
+        self.task_no_header.grid(row=0, column=0)
+
+        self.name_header = Label(
+            self.tasks_frame,
+            text="Task",
+            borderwidth=1, 
+            relief="solid",
+            padx=5
+        )
+        self.name_header.grid(row=0, column=1)
+
+        self.status_header = Label(
+            self.tasks_frame,
+            text="Status",
+            borderwidth=1, 
+            relief="solid",
+            padx=5
+        )
+        self.status_header.grid(row=0, column=2)
         
         self.tasks_frame.grid() # Show
 
-    # Task Entry Frame #
+    #### Task Entry Frame ####
+        self.taskname_entry_label = Label(
+            self.task_entry_frame,
+            text="Task name"
+        )
+        self.taskname_entry_label.grid(row=0, column=0)
+
         self.task_name = StringVar()
         self.taskname_entry = Entry(
             self.task_entry_frame,
             textvariable=self.task_name
         )
-        self.taskname_entry.grid() # Show
-       
+        self.taskname_entry.grid(row=0, column=1) # Show
+
         self.confirm_task = Button(
             self.task_entry_frame,
             text="Add Task",
             command=self.add_task
        )
-        self.confirm_task.grid() # Show
+        self.confirm_task.grid(row=2) # Show
 
-
+## Methods ##
     def add_task(self):
         """
         """
-        # Create new task label widget
-        grid_length = len(self.task_labels)
-        new_task_label = Label(
-            self.tasks_frame,
-            text=self.task_name.get()
-        )
-        new_task_label.grid(row=grid_length + 1, column=0)
-        
-        # Create new checkbox
-        new_task_checkbox = Checkbutton(
-            self.tasks_frame
-        )
-        new_task_checkbox.grid(row=grid_length + 1, column=1)
-
-        # Append to list for association with task label
-        self.task_checkboxes.append(new_task_checkbox)
-        # Adds new task label to task labels list to keep track of
-        self.task_labels.append(new_task_label)
+        self.tasks.append(Task(self.task_name.get()))
+        messagebox.showinfo("Task added", "Task has been succesfully added")
+        self.output_tasks()
         self.switch_frame()
+
+
+    def output_tasks(self):
+        task_counter = 1
+        for task in self.tasks:
+            task_num = task_counter
+            temp_name = task.t_name
+            temp_status = task.status
+
+            task_num_label =Label(
+                self.tasks_frame,
+                text=task_num,
+                borderwidth=1, 
+                relief="solid",
+                padx=5
+            )
+            task_num_label.grid(row=task_counter, column=0)
+
+            task_name_label = Label(
+                self.tasks_frame,
+                text=temp_name,
+                borderwidth=1, 
+                relief="solid",
+                padx=5
+            )
+            task_name_label.grid(row=task_counter, column=1)
+
+            task_status_label = Label(
+                self.tasks_frame,
+                text=temp_status,
+                borderwidth=1, 
+                relief="solid",
+                padx=5
+            )
+            task_status_label.grid(row=task_counter, column=2)
+            task_counter += 1
+
+            self.task_button.grid(row=len(self.tasks) + 1, column=0)
+
+
 
 
     def switch_frame(self):
@@ -122,6 +179,7 @@ if __name__ == "__main__":
     root.title("Project Board")
     app = ProjectBoardGUI(root)
     root.mainloop()
+    
 
 
 
