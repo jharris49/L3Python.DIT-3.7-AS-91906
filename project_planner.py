821621237efe60892511f7bd98ca5f57ae029e9d
jharris49 <22jharris@wakatipu.school.nz>
2026-05-11 20:23:12 -0400

    Added Task Deadline Functionality and UI
    New Features:
    - User can now input a specific deadline as part of a task. This deadline being formatted and outputted. The user input deadline is not yet validated.
    - Deadline functionality and UI uses datetime.
    
    Other:
    - Minor UI changes to house the new deadline functionality.

--- project_planner.py
+++ project_planner.py
@@ -1,15 +1,17 @@
 from tkinter import * 
 from tkinter import messagebox
+import datetime
 
 
 class Task:
     task_id_counter = 1
 
-    def __init__(self, t_name, status="Unstarted", description=None):
+    def __init__(self, t_name, t_deadline, status="Unstarted", description=None):
         self.task_id = Task.task_id_counter
         Task.task_id_counter += 1
 
         self.t_name = t_name
+        self.t_deadline = t_deadline
         self.status = status
         self.description = description
 
@@ -42,7 +44,8 @@ class ProjectBoardGUI:
     # Frames setup
         self.tasks_frame = Frame(parent)
         self.task_entry_frame = Frame(parent)
-        
+        self.task_deadline_frame = Frame(self.task_entry_frame)
+
     #### Tasks Frame ####
         self.task_button = Button(
             self.tasks_frame,
@@ -71,13 +74,22 @@ class ProjectBoardGUI:
             font = "Helvatica 13 bold"
         )
         self.status_header.grid(row=0, column=2, padx=5, pady=5)
+
+        self.deadline_header = Label(
+            self.tasks_frame,
+            text="Deadline",
+            font = "Helvatica 13 bold"
+        )
+        self.deadline_header.grid(row=0, column=3, padx=5, pady=5)
+
         
         self.tasks_frame.grid(padx=20, pady=5) # Show
 
     #### Task Entry Frame ####
         self.taskname_entry_label = Label(
             self.task_entry_frame,
-            text="Task name"
+            text="Task name",
+            font="Helvetica 13 bold"
         )
         self.taskname_entry_label.grid(row=0, column=0)
 
@@ -87,6 +99,64 @@ class ProjectBoardGUI:
             textvariable=self.task_name
         )
         self.taskname_entry.grid(row=0, column=1) # Show
+
+        self.deadline_label = Label(
+            self.task_entry_frame,
+            text="Deadline",
+            font="Helvetica 13 bold"
+        )
+        self.deadline_label.grid(row=1, column=0)
+
+        self.format_label1 = Label(
+            self.task_deadline_frame,
+            text="/"
+        )
+        self.format_label2 = Label(
+            self.task_deadline_frame,
+            text="/"
+        )
+
+        self.format_label1.grid(row=1, column=1)
+        self.format_label2.grid(row=1, column=3)
+
+        current_date = datetime.datetime.now()
+
+        self.task_deadline_day = StringVar()
+        self.task_deadline_day.set(current_date.strftime("%d"))
+        self.deadline_day_entry = Spinbox(
+            self.task_deadline_frame,
+            textvariable=self.task_deadline_day,
+            width=2,
+            from_=1,
+            to=31
+        )
+        self.deadline_day_entry.grid(row=1, column=0)
+
+        self.task_deadline_month = StringVar()
+        self.task_deadline_month.set(current_date.strftime("%m"))
+        self.deadline_month_entry = Spinbox(
+            self.task_deadline_frame,
+            textvariable=self.task_deadline_month,
+            width=2,
+            from_=1,
+            to=12
+        )
+        self.deadline_month_entry.grid(row=1, column=2)
+
+        self.task_deadline_year = StringVar()
+        self.task_deadline_year.set(current_date.strftime("%Y"))
+        self.deadline_year_entry = Entry(
+            self.task_deadline_frame,
+            textvariable=self.task_deadline_year,
+            width=4
+        )
+        self.deadline_year_entry.grid(row=1, column=4)
+        self.task_deadline_frame.grid(row=1, column=1, sticky="w")
+
+        deadline_day = int(self.task_deadline_day.get())
+        deadline_month = int(self.task_deadline_month.get())
+        deadline_year = int(self.task_deadline_year.get())
+        self.saveable_date = datetime.datetime(deadline_year, deadline_month, deadline_day)
 
         self.confirm_task = Button(
             self.task_entry_frame,
@@ -124,6 +194,13 @@ class ProjectBoardGUI:
             font = "Helvatica 13 bold"
         )
         self.status_header.grid(row=0, column=2, padx=5, pady=5)
+
+        self.deadline_header = Label(
+            self.tasks_frame,
+            text="Deadline",
+            font = "Helvatica 13 bold"
+        )
+        self.deadline_header.grid(row=0, column=3, padx=5, pady=5)
         
         self.tasks_frame.grid(padx=20, pady=5) # Show
 
@@ -131,7 +208,7 @@ class ProjectBoardGUI:
     def add_task(self):
         """
         """
-        self.tasks.append(Task(self.task_name.get()))
+        self.tasks.append(Task(self.task_name.get(), self.saveable_date))
         messagebox.showinfo("Task added", "Task has been succesfully added")
         self.output_tasks()
         self.switch_frame(1)
@@ -147,6 +224,7 @@ class ProjectBoardGUI:
             task_num = task.task_id
             temp_name = task.t_name
             temp_status = task.status
+            temp_deadline = task.t_deadline
             self.text_colour = self.status_colours[task.status]
 
             task_num_label =Label(
@@ -173,7 +251,14 @@ class ProjectBoardGUI:
             
             task_status_menu.config(fg=self.text_colour)
             task_status_menu.grid(row=task_counter, column=2)
-
+            
+            formatted_date = f"{temp_deadline.strftime("%d")} {temp_deadline.strftime("%b")} {temp_deadline.strftime("%Y")}"
+            task_deadline_label = Label(
+                self.tasks_frame,
+                text=formatted_date,
+                fg=self.text_colour
+            )
+            task_deadline_label.grid(row=task_counter, column=3, padx=20)
 
             self.remove_task_button = Button(
                 self.tasks_frame,
@@ -181,7 +266,7 @@ class ProjectBoardGUI:
                 command=lambda id=task_num: self.remove_task(id)
             )
 
-            self.remove_task_button.grid(row=task_counter, column=3)
+            self.remove_task_button.grid(row=task_counter, column=4)
 
             self.current_status.trace_add("write", lambda *args, t=task_counter, s=self.current_status: self.update_task(t - 1, s, *args))
             task_counter += 1
